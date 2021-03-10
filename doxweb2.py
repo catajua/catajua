c674305aad836e43454b5f40e71950fa65a90e1d
catajua <75099982+catajua@users.noreply.github.com>
2021-03-10 17:13:37 -0300

    Add files via upload

---
+++ doxweb2.py
@@ -0,0 +1,29 @@
+import webbrowser
+
+print ("DOX WEB BY CATAJUA")
+
+ola = "QUE HERRAMIENTA USARAS"
+
+pto = "1)PHONEINFOGA"
+
+
+tartado = "3)WHO IS LOOKUP"
+
+
+tonto = "2) DOX IP :"
+
+
+
+kpo = input (ola + pto + tonto + tartado)
+
+input (kpo)
+
+if kpo == "1":
+   webbrowser.open("https://numverify.com/")
+
+if kpo == "2":
+    webbrowser.open("https://ipinfo.io/")
+
+if kpo == "3":
+    webbrowser.open("https://whois.domaintools.com/")
+
